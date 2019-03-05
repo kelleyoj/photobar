3ac81ea66cf33e87cfdc5f66d7508725d7551a6d
OJ <kelleyoj@gmail.com>
2019-03-05 13:18:31 -0500

    remove MuiThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Main from "./components/Main";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="app">
-          <Main />
-        </div>
-      </MuiThemeProvider>
+      <div className="app">
+        <Main />
+      </div>
     );
   }
 }
